d1f6122e6af260984e607a8e37ffdd6923129cfc
Jonah Graham <jonah@kichwacoders.com>
2018-09-18 11:42:12 +0100

    Build in the container

--- dockerfiles/theia-usbdebug/build.sh
+++ dockerfiles/theia-usbdebug/build.sh
@@ -11,16 +11,5 @@
 base_dir=$(cd "$(dirname "$0")"; pwd)
 . "${base_dir}"/../build.include
 
-
-if [ ! -d "${base_dir}/theia" ]; then
-  echo "${ERROR}Have you cloned theia into ${base_dir}/theia from https://github.com/theia-ide/theia?"
-  exit 2
-fi
-
-if [ ! -f "${base_dir}/theia/examples/browser/webpack.config.js" ]; then
-  echo "${ERROR}Have you build theia in ${base_dir}/theia by running yarn (see https://github.com/theia-ide/theia/blob/master/doc/Developing.md#quick-start)?"
-  exit 2
-fi
-
 init --name:theia-usbdebug "$@"
 build
